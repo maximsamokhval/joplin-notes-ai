b263b1eaab89d2a7722323e829f791970d1b6438
Maxim Samokhval <maxim.samokhval@gmail.com>
2026-04-10 09:39:10 +0300

    chore(repo): update joplin_notes_ai/services/__init__.py

---
+++ joplin_notes_ai/services/__init__.py
@@ -0,0 +1,8 @@
+from .content_builder import build_final_content, build_related_links_block
+from .note_processing import NoteProcessingService
+
+__all__ = [
+    "build_final_content",
+    "build_related_links_block",
+    "NoteProcessingService",
+]
