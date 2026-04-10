2b25a297a33aaf1ff6196fd9e0f71d05cd1f6a22
Maxim Samokhval <maxim.samokhval@gmail.com>
2026-04-10 13:32:16 +0300

    feat(services): export tag taxonomy service

--- joplin_notes_ai/services/__init__.py
+++ joplin_notes_ai/services/__init__.py
@@ -1,8 +1,10 @@
 from .content_builder import build_final_content, build_related_links_block
 from .note_processing import NoteProcessingService
+from .tag_taxonomy import TagTaxonomyService
 
 __all__ = [
     "build_final_content",
     "build_related_links_block",
     "NoteProcessingService",
+    "TagTaxonomyService",
 ]
